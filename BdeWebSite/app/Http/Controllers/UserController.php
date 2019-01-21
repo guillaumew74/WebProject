0b53f8d499e13cbb19863ec5c78f8e246d646f96
Bnder1 <33345038+Bnder1@users.noreply.github.com>
2019-01-21 21:19:48 +0100

    ressources user

--- BdeWebSite/app/Http/Controllers/UserController.php
+++ BdeWebSite/app/Http/Controllers/UserController.php
@@ -2,83 +2,71 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UserCreateRequest;
+use App\Http\Requests\UserUpdateRequest;
+
+use App\Repositories\UserRepository;
+
 use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
+
+    protected $userRepository;
+
+    protected $nbrPerPage = 4;
+
+    public function __construct(UserRepository $userRepository)
+    {
+        $this->userRepository = $userRepository;
+    }
+
     public function index()
     {
-        //
+        $users = $this->userRepository->getPaginate($this->nbrPerPage);
+        $links = $users->render();
+
+        return view('index', compact('users', 'links'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
-        //
+        return view('create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(UserCreateRequest $request)
     {
-        //
+        $user = $this->userRepository->store($request->all());
+
+        return redirect('user')->withOk("L'utilisateur " . $user->name . " a été créé.");
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function show($id)
     {
-        //
+        $user = $this->userRepository->getById($id);
+
+        return view('show',  compact('user'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function edit($id)
     {
-        //
+        $user = $this->userRepository->getById($id);
+
+        return view('edit',  compact('user'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(UserUpdateRequest $request, $id)
     {
-        //
+        $this->userRepository->update($id, $request->all());
+
+        return redirect('user')->withOk("L'utilisateur " . $request->input('name') . " a été modifié.");
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
     public function destroy($id)
     {
-        //
+        $this->userRepository->destroy($id);
+
+        return back();
     }
+
 }
