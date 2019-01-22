1cbb59242768755aba8d43809c98b48b9b8abe3e
Bnder1 <33345038+Bnder1@users.noreply.github.com>
2019-01-22 16:36:14 +0100

    Create error.blade.php

---
+++ BdeWebSite/resources/views/errors/error.blade.php
@@ -0,0 +1,15 @@
+@extends('templateuser')
+
+@section('contenu')
+    <br>
+    <div class="col-sm-offset-4 col-sm-4">
+    <div class="panel panel-danger">
+      <div class="panel-heading">
+        <h3 class="panel-title">Il y a un problème !</h3>
+      </div>
+      <div class="panel-body">
+        <p>Nous sommes désolés mais il n'y a pas assez d'évènements (+5)...</p>
+      </div>
+    </div>
+  </div>
+@endsection
