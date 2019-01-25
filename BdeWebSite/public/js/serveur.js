a57bc97b113c82b4aa9497affe273e9bc516951e
Bnder1 <33345038+Bnder1@users.noreply.github.com>
2019-01-25 09:44:29 +0100

    node

--- BdeWebSite/public/js/serveur.js
+++ BdeWebSite/public/js/serveur.js
@@ -1,34 +1,34 @@
 //L'application requiert l'utilisation du module Express.
-//La variable express nous permettra d'utiliser les fonctionnalités du module Express.  
+//La variable express nous permettra d'utiliser les fonctionnalités du module Express.
 
-var express = require("express"); 
+var express = require("express");
 // Nous définissons ici les paramètres du serveur.
 
-var hostname = 'localhost'; 
-var port = 8080; 
-var bodyParser = require("body-parser"); 
+var hostname = 'localhost';
+var port = 8080;
+var bodyParser = require("body-parser");
 var cors = require('cors');
-// Nous créons un objet de type Express. 
-var app = express(); 
+// Nous créons un objet de type Express.
+var app = express();
 
 
- 
+
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json()); 
+app.use(bodyParser.json());
 //Afin de faciliter le routage (les URL que nous souhaitons prendre en charge dans notre API), nous créons un objet Router.
-//C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes. 
-var myRouter = express.Router(); 
- 
-// Je vous rappelle notre route (/piscines).  
+//C'est à partir de cet objet myRouter, que nous allons implémenter les méthodes.
+var myRouter = express.Router();
+
+// Je vous rappelle notre route (/piscines).
 myRouter.route('/piscines')
 // J'implémente les méthodes GET, PUT, UPDATE et DELETE
 // GET
-.get(function(req,res){ 
+.get(function(req,res){
 	   res.json({
- message : "Liste les piscines de Lille Métropole avec paramètres :",
+ message : "Ecole",
  ville : req.query.ville,
- nbResultat : (req.query.maxresultat)*5, 
+ nbResultat : (req.query.maxresultat)*5,
  methode : req.method });
 })
 
@@ -40,13 +40,13 @@ myRouter.route('/piscines')
 
 
 // Nous demandons à l'application d'utiliser notre routeur
-app.use(myRouter);  
- 
-// Démarrer le serveur 
+app.use(myRouter);
+
+// Démarrer le serveur
 app.listen(port, hostname, function(){
-	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port); 
+	console.log("Mon serveur fonctionne sur http://"+ hostname +":"+port);
 });
- 
+
 
 
 function doSomething(ecole, res){
@@ -57,18 +57,18 @@ function doSomething(ecole, res){
 
 
 	if (ecole == "cesi") {
-		resultat = "c'est la meilleure ecole du monde";
+		resultat = "C'est la meilleure ecole du monde";
 	}
 
 	else if (ecole == "epitech") {
-		resultat = "alors pas trop noyer dans ta piscine ?";
+		resultat = "Alors pas trop noyer dans ta piscine ?";
 	}
 
 	else{
-		resultat = "viens au cesi l'année prochaine";
+		resultat = "Viens au cesi l'année prochaine";
 	}
 
-		res.json({message : resultat, 
+		res.json({message : resultat,
 
  		});
 
--- BdeWebSite/resources/views/vue/api.blade.php
+++ BdeWebSite/resources/views/vue/api.blade.php
@@ -1,13 +1,32 @@
 
 <!DOCTYPE html>
 <html>
- 
-  Ecole:<br>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Admin BDE WEB SITE</title>
+    {!! Html::style('https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css') !!}
+    {!! Html::style('https://netdna.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css') !!}
+    <!--[if lt IE 9]>
+      {{ Html::style('https://oss.maxcdn.com/libs/html5shiv/3.7.2/html5shiv.js') }}
+      {{ Html::style('https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js') }}
+    <![endif]-->
+    <style> textarea { resize: none; }
+    div{
+      margin-left: 5%;
+      margin-top: 5%;
+    }
+
+  </style>
+</head>
+<div>
+  <h1>&Eacute;cole:</h1>
   <input id = "ecole" type="text" name="ecole" value="ecole"><br><br>
-  <input type="submit" value="Submit"  onclick=" test()">
+  <input type="submit" value="Envoyer"  onclick=" test()">
 
-<p id ="test"></p>
-
+<h3 id ="test"></h3>
+</div>
 </html>
 
 
@@ -15,8 +34,8 @@
 <script>
 
 function test(){
-  
- 
+
+
 var cesi = document.getElementById("ecole").value;
 var http = new XMLHttpRequest();
 var url = 'http://localhost:8080/piscines';
@@ -28,9 +47,9 @@ http.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
 
   http.onreadystatechange = function() {
     if (this.readyState == 4 && this.status == 200) {
-	
+
 		console.log(this.response);
-	
+
       document.getElementById("test").innerHTML = (JSON.parse(this.response)).message;
     }
   };
