31413dcab0b8e6fef2bac65d44005b5c09bf741e
Bnder1 <33345038+Bnder1@users.noreply.github.com>
2019-01-28 14:15:57 +0100

    css + card

--- BdeWebSite/resources/views/mag/pan.blade.php
+++ BdeWebSite/resources/views/mag/pan.blade.php
@@ -1,79 +1,31 @@
- 
-@extends('template')
+ @extends('template')
 
 @section('contenu')
 
 
-<!DOCTYPE html>
-<html lang="en">
+<div id="wrapper">
+<!-- Page Content -->
+<div class="page-content-wrapper">
 
-<head>
+    @for ($i = 0; $i < 6; $i++)
 
-  <meta charset="utf-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-  <meta name="description" content="Shop of bdewebsite">
-  <meta name="author" content="">
+      <div class="card conteneur" style="width: 18rem;">
 
-  <title>Shop</title>
-
-  <!-- Bootstrap core CSS -->
-  <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
-
-  <!-- Custom styles for this template -->
-  <link href="css/shop-homepage.css" rel="stylesheet">
-
-</head>
-
-<body>
-
-
-
-  <!-- Page Content -->
-  <div class="container">
-
-    @for ($i = 0; $i < 4; $i++)
-    
-
-    <div class="row">
-      <div class="col-lg-4 col-md-6 mb-4">
-        <div class="card h-100">
-          <a href="#"><img class="card-img-top" src="https://cdn1.ideecadeau.fr/media/catalog/product/l/a/la-tasse-magique-ideecadeau-fr_5451-4ed425a6.jpg" alt=""></a>
-          <div class="card-body">
-            <h4 class="card-title">
-              <a href="/buy/1"> wdkfj</a>
-            </h4>
-            <h5> test €</h5>
-            <p class="card-text">test</p>
-          </div>
-          <div class="card-footer">
-            <small class="text-muted">&#9733; &#9733; &#9733; &#9733; &#9734;</small>
-          </div>
+        <img style="width:50%; " class="card-img-top contenu" src="https://cdn1.ideecadeau.fr/media/catalog/product/l/a/la-tasse-magique-ideecadeau-fr_5451-4ed425a6.jpg" alt="">
+        <div class="card-body">
+          <h5 class="card-title">Prix: X €</h5>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <a href="/buy/1" class="btn btn-primary">Go somewhere</a>
         </div>
       </div>
-    </div>
+
+
     <!-- /.row -->
     @endfor
 
   </div>
-
-
 </div>
-<!-- /.container -->
-
-
-<!-- /.container -->
-</footer>
-
-<!-- Bootstrap core JavaScript -->
-<script src="vendor/jquery/jquery.min.js"></script>
-<script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
-
-</body>
-
-</html>
-
-
-
-
 
 @endsection
+
+
