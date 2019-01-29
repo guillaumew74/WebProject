16b123027d023b377dc3074f3a2a6abf3b6276f3
nielsboeckx1 <33620064+nielsboeckx1@users.noreply.github.com>
2019-01-29 15:40:21 +0100

    Ajout panier vide

---
+++ BdeWebSite/resources/views/mag/emptyPan.blade.php
@@ -0,0 +1,28 @@
+@extends('templateuser')
+
+
+@section('contenu')
+
+<br>
+
+
+<div class="col-sm-offset-3 col-sm-6">
+
+	<div class="panel panel-danger">
+
+		<div class="panel-heading">Erreur</div>
+
+		<div class="panel-body">
+
+			Votre panier est vide merci de retourner vers la boutique.
+			
+			<br>
+
+			<a href="/boutique">Boutique</a>
+		</div>
+
+	</div>
+</div>
+
+
+@endsection
